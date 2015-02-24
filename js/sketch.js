7e1cd11b30d8eda31aa45e0d58214bfb375dd8e2
codymckeon@gmail.com <codymckeon@gmail.com>
2015-02-23 18:24:27 -0800

    update colorpicker and clear grid

--- js/sketch.js
+++ js/sketch.js
@@ -21,6 +21,7 @@ function clearGrid(){
 function newGrid(){
     var num_Dimension = prompt("Please enter a number between 1 and 64. The number you choose will determine the widht and height of the grid you decide to be artsy farsty on.")
     addDivs(num_Dimension);
+    size = num_Dimension;
     if(num_Dimension > 0 && num_Dimension < 65){
       if(num_Dimension % 1 === 0){
         addDivs(num_Dimension);
@@ -40,22 +41,22 @@ function newGrid(){
 //function is for click and then sketch feature
 function sketch(){
   $('.grid').on("mouseover", function(){
-          $(this).css("background", "#FF6600");
+          $(this).css("background", "#00ff00");
       });
 };
 
-
+var size = 16;
 $(document).ready(function(){
-  addDivs(16);
 
 
+addDivs(size);
   $('.grid').on('click', function(){
       sketch();
   });
 
   $(".clearGrid").click(function(){
       clearGrid();
-      newGrid()
+       addDivs(size);
       $('.grid').on('click', function(){
       sketch();
     });
@@ -69,30 +70,43 @@ $(document).ready(function(){
     });
   });
 
-  alert("work");
+  //alert("work");
     var canvas = document.getElementById('canvas_picker').getContext('2d');
     var img = new Image();
     img.src = "colorwheel.png";
 
     //copy the image to the canvas
     $(img).load(function(){
-        alert("load");
-        canvas.drawImage(img,0,0);
+        //alert("load");
+        canvas.drawImage(img,0,0,150,150);
     });
+
+
+    // http://www.javascripter.net/faq/rgbtohex.htm
+  function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
+  function toHex(n) {
+    n = parseInt(n,10);
+    if (isNaN(n)) return "00";
+    n = Math.max(0,Math.min(n,255));
+    return "0123456789ABCDEF".charAt((n-n%16)/16)  + "0123456789ABCDEF".charAt(n%16);
+  }
 
     $('#canvas_picker').click(function(event){
         //get coordinates
         var x = event.pageX - this.offsetLeft;
         var y = event.pageY - this.offsetTop;
+        //alert(x + "pageY" + y);
         //getting image data and RGB values
-        var img_data = canvas.getImageData(x,y,1,1).data;
+        var img_data = canvas.getImageData(x,y,3,3).data;
         var R = img_data[0];
         var G = img_data[1];
         var B = img_data[2];
         var rgb = R + ',' + G + ',' + B;
-
+        var hex = rgbToHex(R,G,B);
+        alert(rgb);
+        $('.grid').unbind('mouseover');
         $('.grid').on('mouseover', function(){
-            $(this).css("background", "rgb");
+            $(this).css("background", "#"+hex);
         });
       });
 
